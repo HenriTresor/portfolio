21a4f9328a4d35e4c594562bbbb7cfd04ea5502b
henritresor <shimwamanahenritresor@gmail.com>
2023-04-27 17:08:37 +0200

    added loading component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
+import React from 'react'
 import Body from "./Components/Body";
 import Contacts from "./Components/Contacts";
 import Header from "./Components/Header";
+import Loading from "./Components/Loading";
 import Skills from "./Components/Skills";
 import Works from "./Components/Works";
 
 export default function App() {
+
+  // eslint-disable-next-line no-constant-condition
   return (
-    <>
+    <React.Suspense  fallback={<Loading />}>
       <Header />
       <Body />
       <Skills />
       <Works />
       <Contacts />
-    </>
+    </React.Suspense>
  )
 }
---
+++ src/Components/Loading.jsx
@@ -0,0 +1,20 @@
+// import React from 'react'
+import {motion} from 'framer-motion'
+
+
+const Loading = () => {
+  return (
+      <div
+          className="loader"
+      >
+          <motion.div
+            
+          className="loading-component"
+          >
+              
+          </motion.div>
+      </div>
+  )
+}
+
+export default Loading
