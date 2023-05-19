15042b4ffe036d00412786918b44b04e2403783c
henritresor <shimwamanahenritresor@gmail.com>
2023-05-19 10:08:16 +0200

    added suspense

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import {Suspense} from 'react'
 import Body from "./Components/Body";
 import Contacts from "./Components/Contacts";
 import Header from "./Components/Header";
@@ -10,12 +10,12 @@ export default function App() {
 
   // eslint-disable-next-line no-constant-condition
   return (
-    <React.Suspense  fallback={<Loading />}>
+    <Suspense  fallback={<Loading />}>
       <Header />
       <Body />
       <Skills />
       <Works />
       <Contacts />
-    </React.Suspense>
+    </Suspense>
  )
 }
