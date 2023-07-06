e98ec4d6ab3d742c6321082e26dbd51f75409bbc
henritresor <shimwamanahenritresor@gmail.com>
2023-07-06 21:39:00 +0200

    added navigation in the header

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div className=' bg-[#081b29] h-max-screen'>
       <Header />
       <div className='w-full flex h-screen justify-around p-5 items-center'>
-        <div className='w-[50%] flex flex-col gap-4 text-left ml-5'>
+        <div className='w-[50%] flex flex-col gap-4 text-left ml-20'>
           <h1 className='text-[#ededed] text-[2em]'>Hi, I am Henri Tresor</h1>
           <h4 className='text-[#00abf0] font-bold tracking-widest text-[1.4em]'>Full-stack engineer</h4>
           <p className='text-[#ededed] text-left tracking-wider'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et modi ducimus sint vitae quisquam recusandae odio atque nihil ea minima similique in ab reprehenderit nesciunt, voluptas cumque molestias. Facilis, voluptate.</p>
@@ -15,7 +15,7 @@ export default function Home() {
             <button className='btn text-[#00abf0]'>Let&apos;s talk</button>
           </div>
         </div>
-        <div className='w-[50%] flex justify-center gap-2'>
+        <div className='w-[50%] hidden justify-center gap-2 md:flex'>
           <div className='w-[100px] h-[100px] bg-sky-500 rounded-full scale-50'>
 
           </div>
--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'next/link'
 
 type Props = {}
 
@@ -7,15 +8,23 @@ const Header = (props: Props) => {
       <div
           className='text-[#ededed] flex justify-around pt-4 w-full'
       >
-          <h2 className='text-2xl font-bold'>
-              
-              Henri.</h2>
+          <Link href={'/'}>
+              <h2 className='text-2xl font-bold'>
+
+                  Henri.</h2>
+          </Link>
           <div className=''>
               <ul className='flex gap-9'>
-                  <li className='nav-list-item'>Home</li>
-                  <li className='nav-list-item'>About</li>
-                  <li className='nav-list-item'>Education</li>
-                  <li className='nav-list-item'>Contacts</li>
+                  <Link href={'/'}><li className='nav-list-item'>Home</li></Link>
+                  <Link href={'/about'}>
+                      <li className='nav-list-item'>About</li>
+                  </Link>
+                  <Link href={'/education'}>
+                      <li className='nav-list-item'>Education</li>
+                  </Link>
+                  <Link href={'/contacts'}>
+                      <li className='nav-list-item'>Contacts</li>
+                  </Link>
               </ul>
           </div>
       </div>
