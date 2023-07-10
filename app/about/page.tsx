e08412eb31f2708c27c589793663f701c08d710f
henritresor <shimwamanahenritresor@gmail.com>
2023-07-10 21:16:41 +0200

    made some changes

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -37,7 +37,7 @@ const About = (props: Props) => {
                     <div className='progress w-full mt-4 p-2' key={skill.skil}>
                       <span className='flex justify-between mb-2 '>
                         <h1>{skill.skil}</h1>
-                        <h1>{skill.level}%</h1>
+                        <h1>{skill.level}</h1>
                       </span>
                       <div className='relative w-full p-1 outline outline-2 outline-[#00abf0] overflow-hidden rounded-lg'>
                         <div className={`absolute left-0 w-[${skill.level}] h-full bg-[#00abf0] top-0 `}></div>
@@ -56,10 +56,10 @@ const About = (props: Props) => {
                     <div className='progress w-full mt-4 p-2' key={skill.skill}>
                       <span className='flex justify-between mb-2 '>
                         <h1>{skill.skill}</h1>
-                        <h1>{skill.level}%</h1>
+                        <h1>{skill.level}</h1>
                       </span>
                       <div className='relative w-full p-1 outline outline-2 outline-[#00abf0] overflow-hidden rounded-lg'>
-                        <div className={`absolute left-0 w-[${skill.level}] h-full bg-[#00abf0] top-0 `}></div>
+                        <div className={`absolute left-0 w-[${(skill.level)}] h-full bg-[#00abf0] top-0 `}></div>
                       </div>
                     </div>
                   ))
