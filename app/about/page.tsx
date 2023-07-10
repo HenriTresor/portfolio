19d321d124e8be7177c685f2a541ef2a51b9fe14
henritresor <shimwamanahenritresor@gmail.com>
2023-07-10 21:06:23 +0200

    added professional skills

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,29 +1,29 @@
 import Header from '@/components/Header'
 import React from 'react'
 import Image from 'next/image'
-import skills from '@/data/skills'
+import { CodingSKills, professionalSkills } from '@/data/skills'
 
 type Props = {}
 
 const About = (props: Props) => {
   return (
-      <div className=' bg-[#081b29] h-auto '>
-          <Header />
-          <div className='h-auto flex flex-col p-5 justify-start mt-20 items-center gap-10'>
-              <h1 className='text-[3em] text-white'>About <span className='text-[#00abf0]'>Me</span></h1>
-              <Image
-                  src={'/coding.jpg'}
-                  alt={'coding image'}
-                  width={'100'}
-                  height={'100'}
-                  className='rounded-full h-[200px] w-[200px] object-fill outline-7 outline-[#00abf0] outline outline-offset-4'
+    <div className=' bg-[#081b29] h-auto '>
+      <Header />
+      <div className='h-auto flex flex-col p-5 justify-start mt-20 items-center gap-10'>
+        <h1 className='text-[3em] text-white'>About <span className='text-[#00abf0]'>Me</span></h1>
+        <Image
+          src={'/coding.jpg'}
+          alt={'coding image'}
+          width={'100'}
+          height={'100'}
+          className='rounded-full h-[200px] w-[200px] object-fill outline-7 outline-[#00abf0] outline outline-offset-4'
         />
-        
+
         <p className='text-slate-100 text-left p-10 ml-3 mr-3 mt-5 tracking-wider leading-7 '>
           I am a skilled software engineer with a strong foundation in coding and a passion for creating innovative solutions. With expertise in languages such as Python and C, I have successfully developed and deployed robust software applications. My proficiency extends to database management, web development, and software testing, ensuring seamless functionality and user satisfaction. Adaptable and detail-oriented, I thrive in collaborative environments where I can leverage my problem-solving skills to overcome complex challenges. Committed to staying at the forefront of technological advancements, I continuously seek opportunities to expand my knowledge and contribute to cutting-edge projects. With a proven track record of delivering high-quality software solutions, I am ready to make a valuable impact in the field of software engineering.
         </p>
 
-        <div className='text-slate-100 w-full text-center'>  
+        <div className='text-slate-100 w-full text-center'>
           <h1 className='text-[3em] text-white'>My <span className='text-[#00abf0]'>Skills</span></h1>
 
           <div className='flex justify-center items-start w-full mt-10'>
@@ -31,11 +31,11 @@ const About = (props: Props) => {
               <h1>Coding Skills</h1>
               <div className='box w-full'>
                 {
-                 skills && skills.map(skill => (
+                  CodingSKills && CodingSKills.map(skill => (
                     <div className='progress w-full mt-4 p-2' key={skill.skil}>
                       <span className='flex justify-between mb-2 '>
-                        <h1>{ skill.skil}</h1>
-                        <h1>{ skill.level}%</h1>
+                        <h1>{skill.skil}</h1>
+                        <h1>{skill.level}%</h1>
                       </span>
                       <div className='relative w-full p-1 outline outline-2 outline-[#00abf0] overflow-hidden rounded-lg'>
                         <div className={`absolute left-0 w-[${skill.level}%] h-full bg-[#00abf0] top-0 `}></div>
@@ -49,13 +49,25 @@ const About = (props: Props) => {
             <div className='ml-10 w-full'>
               <h1>Professional Skills</h1>
               <div className='box w-full'>
-               
+                {
+                  professionalSkills && professionalSkills.map(skill => (
+                    <div className='progress w-full mt-4 p-2' key={skill.skill}>
+                      <span className='flex justify-between mb-2 '>
+                        <h1>{skill.skill}</h1>
+                        <h1>{skill.level}%</h1>
+                      </span>
+                      <div className='relative w-full p-1 outline outline-2 outline-[#00abf0] overflow-hidden rounded-lg'>
+                        <div className={`absolute left-0 w-[${skill.level}%] h-full bg-[#00abf0] top-0 `}></div>
+                      </div>
+                    </div>
+                  ))
+                }
               </div>
             </div>
           </div>
 
         </div>
-         </div>
+      </div>
     </div>
   )
 }
--- data/skills.ts
+++ data/skills.ts
@@ -1,44 +1,62 @@
-const skills = [
-    {
-        level: '99',
-        skil:"HTML"
-    },
-    {
-        level: '99',
-        skil:"CSS"
-    },
-    {
-        level: '99',
-        skil:"Tailwind"
-    },
-    {
-        level: '99',
-        skil:"Bootstrap"
-    },
-    {
-        level: '99',
-        skil:"Vanilla JavaScript"
-    },
-    {
-        level: '93',
-        skil:"ReactJS"
-    },
-    {
-        level: '90',
-        skil:"NextJS"
-    },
-    {
-        level: '94',
-        skil:"Nodejs"
-    },
-    {
-        level: '99',
-        skil:"MongoDB"
-    },
-    {
-        level: '95',
-        skil:"PHP"
-    },
-]
+const CodingSKills = [
+  {
+    level: "99",
+    skil: "HTML",
+  },
+  {
+    level: "99",
+    skil: "CSS",
+  },
+  {
+    level: "99",
+    skil: "Tailwind",
+  },
+  {
+    level: "99",
+    skil: "Bootstrap",
+  },
+  {
+    level: "99",
+    skil: "Vanilla JavaScript",
+  },
+  {
+    level: "93",
+    skil: "ReactJS",
+  },
+  {
+    level: "90",
+    skil: "NextJS",
+  },
+  {
+    level: "94",
+    skil: "Nodejs",
+  },
+  {
+    level: "99",
+    skil: "MongoDB",
+  },
+  {
+    level: "95",
+    skil: "PHP",
+  },
+];
 
-export default  skills
+const professionalSkills = [
+  {
+    skill: "Web development",
+    level: "95",
+  },
+  {
+    skill: "Web design",
+    level: "90",
+  },
+  {
+    skill: "SEO marketing",
+    level: "87",
+  },
+  {
+    skill: "Graphic Design",
+    level: "80",
+  },
+];
+export { CodingSKills, professionalSkills };
