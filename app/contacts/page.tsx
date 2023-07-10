eca5c0a5a842ce64b379b5c96fbd88b25b87b873
henritresor <shimwamanahenritresor@gmail.com>
2023-07-10 20:34:37 +0200

    edited contacts section

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -1,10 +1,17 @@
+import Header from '@/components/Header'
 import React from 'react'
 
 type Props = {}
 
 const Contacts = (props: Props) => {
   return (
-    <div>Contacts</div>
+
+    <div className='bg-[#081b29] h-max-screen'>
+      <Header />
+      <div className='h-screen flex flex-col p-5 justify-start mt-20 items-center gap-10 text-slate-100'>
+        <h1 className='text-[2em]'>Contact <span className='text-[#00baf0]'>Me</span></h1>
+      </div>
+    </div>
   )
 }
 
