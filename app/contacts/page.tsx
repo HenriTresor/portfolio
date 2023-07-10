2e4893b4f4d73b8966498e5c36c2d70383588c98
henritresor <shimwamanahenritresor@gmail.com>
2023-07-10 22:33:50 +0200

    added first contact section

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -8,8 +8,20 @@ const Contacts = (props: Props) => {
 
     <div className='bg-[#081b29] h-max-screen'>
       <Header />
-      <div className='h-screen flex flex-col p-5 justify-start mt-20 items-center gap-10 text-slate-100'>
+      <div className='h-screen flex flex-col p-5 justify-start mt-20 items-center gap-10'>
         <h1 className='text-[2em]'>Contact <span className='text-[#00baf0]'>Me</span></h1>
+
+        <div className='flex flex-col gap-5'>
+          <div>
+            <input placeholder='Enter your name'/>  
+            <input placeholder='enter your email'/>  
+          </div>
+          <input placeholder='Email subject'/>
+          <textarea placeholder='Your message' rows={10}>
+
+          </textarea>
+          <button className='btn text-black bg-[#00abf0]'>send</button>
+        </div>
       </div>
     </div>
   )
