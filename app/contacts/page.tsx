575db271deec1272b6bb911c665694262f652581
henritresor <shimwamanahenritresor@gmail.com>
2023-07-10 22:55:17 +0200

    added more responsiveness

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -7,17 +7,17 @@ type Props = {}
 const Contacts = (props: Props) => {
   return (
 
-    <div className='bg-[#081b29] h-max-screen'>
+    <div className='bg-[#081b29] h-min-screen'>
       <Header />
-      <div className='h-screen flex flex-col p-5 justify-start mt-20 items-center gap-10 text-white text-center'>
+      <div className='h-max-screen md:h-screen flex flex-col p-5 justify-start mt-20 items-center gap-10 text-white text-center'>
         <h1 className='text-[2em] text-white'>Contact <span className='text-[#00baf0]'>Me</span></h1>
 
         <div className='flex flex-col gap-5'>
           <div>
-            <input placeholder='Enter your name' type='text' name='name' />  
-            <input placeholder='enter your email'type='email' name='email'/>  
+            <input placeholder='Enter your name' type='text' name='name' />
+            <input placeholder='enter your email' type='email' name='email' />
           </div>
-          <input placeholder='Email subject'type='text' name='subject'/>
+          <input placeholder='Email subject' type='text' name='subject' />
           <textarea placeholder='Your message' rows={10} name='message'>
 
           </textarea>
