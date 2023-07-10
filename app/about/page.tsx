74eb280dbd5c09c204a8aec4214b9809b0d1be8c
henritresor <shimwamanahenritresor@gmail.com>
2023-07-10 21:14:33 +0200

    made some changes

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -40,7 +40,7 @@ const About = (props: Props) => {
                         <h1>{skill.level}%</h1>
                       </span>
                       <div className='relative w-full p-1 outline outline-2 outline-[#00abf0] overflow-hidden rounded-lg'>
-                        <div className={`absolute left-0 w-[${skill.level}%] h-full bg-[#00abf0] top-0 `}></div>
+                        <div className={`absolute left-0 w-[${skill.level}] h-full bg-[#00abf0] top-0 `}></div>
                       </div>
                     </div>
                   ))
@@ -59,7 +59,7 @@ const About = (props: Props) => {
                         <h1>{skill.level}%</h1>
                       </span>
                       <div className='relative w-full p-1 outline outline-2 outline-[#00abf0] overflow-hidden rounded-lg'>
-                        <div className={`absolute left-0 w-[${skill.level}%] h-full bg-[#00abf0] top-0 `}></div>
+                        <div className={`absolute left-0 w-[${skill.level}] h-full bg-[#00abf0] top-0 `}></div>
                       </div>
                     </div>
                   ))
--- app/data/skills.ts
+++ app/data/skills.ts
@@ -1,42 +1,42 @@
 const CodingSKills = [
   {
-    level: "99",
+    level: "99%",
     skil: "HTML",
   },
   {
-    level: "99",
+    level: "99%",
     skil: "CSS",
   },
   {
-    level: "99",
+    level: "99%",
     skil: "Tailwind",
   },
   {
-    level: "99",
+    level: "99%",
     skil: "Bootstrap",
   },
   {
-    level: "99",
+    level: "99%",
     skil: "Vanilla JavaScript",
   },
   {
-    level: "93",
+    level: "93%",
     skil: "ReactJS",
   },
   {
-    level: "90",
+    level: "90%",
     skil: "NextJS",
   },
   {
-    level: "94",
+    level: "94%",
     skil: "Nodejs",
   },
   {
-    level: "99",
+    level: "99%",
     skil: "MongoDB",
   },
   {
-    level: "95",
+    level: "95%",
     skil: "PHP",
   },
 ];
@@ -44,19 +44,19 @@ const CodingSKills = [
 const professionalSkills = [
   {
     skill: "Web development",
-    level: "95",
+    level: "95%",
   },
   {
     skill: "Web design",
-    level: "90",
+    level: "90%",
   },
   {
     skill: "SEO marketing",
-    level: "87",
+    level: "87%",
   },
   {
     skill: "Graphic Design",
-    level: "80",
+    level: "80%",
   },
 ];
 export { CodingSKills, professionalSkills };
