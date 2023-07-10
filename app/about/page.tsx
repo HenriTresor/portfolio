42875902d92f1797ccbe4cd7ec0281fd5337fd09
henritresor <shimwamanahenritresor@gmail.com>
2023-07-10 20:15:03 +0200

    added about me paragraph

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -16,7 +16,11 @@ const About = (props: Props) => {
                   width={'100'}
                   height={'100'}
                   className='rounded-full h-[200px] w-[200px] object-fill outline-7 outline-[#00abf0] outline outline-offset-4'
-              />
+        />
+        
+        <p className='text-slate-100 text-left p-10 ml-3 mr-3 mt-5 tracking-widest leading-7 '>
+          I am a skilled software engineer with a strong foundation in coding and a passion for creating innovative solutions. With expertise in languages such as Python and C, I have successfully developed and deployed robust software applications. My proficiency extends to database management, web development, and software testing, ensuring seamless functionality and user satisfaction. Adaptable and detail-oriented, I thrive in collaborative environments where I can leverage my problem-solving skills to overcome complex challenges. Committed to staying at the forefront of technological advancements, I continuously seek opportunities to expand my knowledge and contribute to cutting-edge projects. With a proven track record of delivering high-quality software solutions, I am ready to make a valuable impact in the field of software engineering.
+        </p>
          </div>
     </div>
   )
